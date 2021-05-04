66fc7b699c98b459857f1a51de15a803e404c20c
Phoenix <leenguyenlhp@gmail.com>
2021-05-04 23:38:24 +0700

    Test Push

--- GUI_QuanLy/Program.cs
+++ GUI_QuanLy/Program.cs
@@ -18,6 +18,7 @@ namespace GUI_QuanLy
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.WriteLine("Test push");
         }
     }
 }
